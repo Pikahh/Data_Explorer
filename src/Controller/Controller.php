61767c146ae65173f88d0c5fb35be54b02ea25c6
Julien <julien.guy41@hotmail.com>
2019-01-22 09:47:01 +0100

    initialisation

---
+++ src/Controller/Controller.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+class Controller {
+
+	private $container;
+
+	public function __construct($container) {
+		$this->container = $container;
+	}
+
+	public function render($response, $file, $args = []) {
+		return $this->container->view->render($response, $file, $args);
+	}
+
+	public function flash() {
+		return $this->container->flash;
+	}
+
+	public function logger() {
+		return $this->container->logger;
+	}
+
+	public function csrf() {
+
+		return $this->container->csrf;
+	}
+
+	public function mail() {
+		return $this->container->mail;
+	}
+
+	public function router() {
+		return $this->container->router;
+	}
+}
